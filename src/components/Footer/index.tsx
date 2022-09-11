51a24cd36e0359128fc8dfab9184eb9d5c098b72
Felipe Diniz <dinizdev@pop-os.localdomain>
2022-09-10 22:38:30 -0300

    changes with to by

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export  default function Footer(){
     return(
         <div className="flex justify-center ... bg-primary p-4 w-full">
-           <p>Made with Felipe Diniz ❤</p>
+           <p>Made by Felipe Diniz ❤</p>
         </div>
     )
 }
