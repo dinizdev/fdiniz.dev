6047c2b66a3fd66c0b693cec2352bc1088cd479b
Felipe Diniz <dinizdevmaster@gmail.com>
2022-10-07 21:40:37 -0300

    fix colors

--- src/components/FormMail/index.tsx
+++ src/components/FormMail/index.tsx
@@ -52,7 +52,7 @@ export default function FormMail() {
         <div className="md:flex md:items-center">
           <div className="md:w-1/3">
             <button
-              className="focus:shadow-outline rounded bg-green-500 px-60 py-2 font-bold text-white shadow hover:bg-green-600 focus:outline-none sm:min-w-fit"
+              className="focus:shadow-outline rounded bg-green-500 py-2 px-[140px] font-bold text-white shadow hover:bg-green-600 focus:outline-none md:px-[230px]"
               type="submit"
             >
               Enviar
